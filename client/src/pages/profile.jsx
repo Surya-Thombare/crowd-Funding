d8d5fd48ba13f356c12ab8dfd03480736a9bdb8a
Suryakant-thombre-au8 <suryathoombre000@gmail.com>
2023-02-02 22:01:14 +0530

    FunCard completed Loader component Created Fund Campaign Functionality created Campaign Details page created

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components";
 
-const profile = () => {
+const Profile = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const {address, contract, getUserCampaigns} = useStateContext()
+
+  const fetchCampaign = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if(contract) fetchCampaign();
+  }, [address, contract])
+  
+
   return (
-    <div>profile</div>
+    <DisplayCampaigns
+    title="All Campaigns"
+    isLoading={isLoading}
+    campaigns={campaigns}
+    />
   )
 }
 
-export default profile
+export default Profile
